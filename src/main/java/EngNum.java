35b2d16884c6aabe1463386bf7c8b7e5d52f233d
ryeon9445 <ryeon9445@gmail.com>
2018-06-01 11:00:15 +0900

    Add : testCase Modify : code refactoring

--- src/main/java/EngNum.java
+++ src/main/java/EngNum.java
@@ -7,9 +7,14 @@ public class EngNum
 {
     public String sort(final String input)
     {
+        if (input == null)
+        {
+            throw new IllegalArgumentException("do not null");
+        }
+
         if (input.isEmpty())
         {
-            throw new IllegalArgumentException("not empty");
+            throw new IllegalArgumentException("do not empty");
         }
 
         if (!input.matches("[a-zA-Z0-9]*"))
@@ -17,32 +22,33 @@ public class EngNum
             throw new IllegalArgumentException("only english or number");
         }
 
+        String sortedEng = sortByPattern(input, "[^a-zA-Z]*");
+        String sortedNum = sortByPattern(input, "[^\\d]*");
+
         StringBuilder output = new StringBuilder();
-        char[] strings = input.replaceAll("[^a-zA-Z]*", "").toCharArray();
-        Arrays.sort(strings);
-        String sortedString = new String(strings);
-
-        char[] numbers = input.replaceAll("[^\\d]*", "").toCharArray();
-        Arrays.sort(numbers);
-        String sortedNumber = new String(numbers);
-
         for (int i = 0; i < input.length(); i++)
         {
-            if (i > sortedString.length()-1 && i > sortedNumber.length()-1)
+            if (i > sortedEng.length()-1 && i > sortedNum.length()-1)
             {
                 break;
             }
 
-            if (i < sortedString.length())
+            if (i < sortedEng.length())
             {
-                output.append(sortedString.charAt(i));
+                output.append(sortedEng.charAt(i));
             }
-            if (i < sortedNumber.length())
+            if (i < sortedNum.length())
             {
-                output.append(sortedNumber.charAt(i));
+                output.append(sortedNum.charAt(i));
             }
         }
-
         return output.toString();
+    }
+
+    private String sortByPattern(final String input, final String pattern)
+    {
+        char[] strings = input.replaceAll(pattern, "").toCharArray();
+        Arrays.sort(strings);
+        return new String(strings);
     }
 }
--- src/test/java/EngNumTest.java
+++ src/test/java/EngNumTest.java
@@ -23,10 +23,20 @@ public class EngNumTest
         Assert.assertEquals("acyz", engNum.sort("zyca"));
         Assert.assertEquals("a0b7z9", engNum.sort("79az0b"));
 
+        Assert.assertEquals("aadeffiijjosww", engNum.sort("sadfjaiwfjioew"));
+        Assert.assertEquals("00111222333999", engNum.sort("12391023901293"));
         Assert.assertEquals("a0d2d3e3k4k9o9opsvw", engNum.sort("dovkwpoek3093249asd"));
         Assert.assertEquals("a0a0a0a0a0a0a0d0e0e2e2e2f2f2f2f3f3f3f3i3i3i3i4i4j8j8j9j9j9j9k9l9l9o9o9qqrrsswwww",
                 engNum.sort("sdlkfjalsjfiweajfioawejfoiae9230482390483902rqj0a9fjaw09302390932ir09q23if09awef"));
 
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullException()
+    {
+        EngNum engNum = new EngNum();
+        engNum.sort(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
